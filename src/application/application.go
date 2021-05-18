65e03474544d7f630df20332cbad30d660c4d9d5
tan nguyen <tannpv@gmail.com>
2021-05-19 01:26:12 +0700

    update full from udemy

---
+++ src/application/application.go
@@ -0,0 +1,34 @@
+package application
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/tannpv/bookstore_items-api/src/clients/elasticsearch"
+	"net/http"
+	"time"
+
+	"github.com/tannpv/bookstore_utils-go/logger"
+)
+
+var (
+	router = mux.NewRouter()
+)
+
+func StartApplication() {
+	elasticsearch.Init()
+
+	mapUrls()
+
+	srv := &http.Server{
+		Addr: ":8081",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: 500 * time.Millisecond,
+		ReadTimeout:  2 * time.Second,
+		IdleTimeout:  60 * time.Second,
+		Handler:      router,
+	}
+
+	logger.Info("about to start the application...")
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
---
+++ src/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/tannpv/bookstore_items-api/src/application"
+)
+
+func main() {
+	application.StartApplication()
+}
